d3c504595d56229d11986eebd5577e8e9c15f31f
sepehrchn <146048930+sepehrchn@users.noreply.github.com>
2024-06-14 22:11:05 +0800

    Added images

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -67,7 +67,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       .createUserWithEmailAndPassword(
                     email: _emailTextController.text,
                     password: _passwordTextController.text,
-                    date: _dateTextController.text,
+                    //date: _dateTextController.text,
                   )
                       .then((value) {
                     print("Created New Account");
